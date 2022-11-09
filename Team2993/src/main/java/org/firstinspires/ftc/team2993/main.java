fac9a47883be7907a309390a5d1f24e00488cf7e
Nikhil Gajjar <nikgajjar51@gmail.com>
2022-11-09 12:57:27 -0500

    Motor Config Done

---
+++ Team2993/src/main/java/org/firstinspires/ftc/team2993/main.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.team2993;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class driver_control extends OpMode{
+    public robot_config robot;
+    @Override
+    public void init(){
+        robot = new robot_config(hardwareMap);
+
+    }
+    public void init_loop(){}
+    public void start(){}
+    public void loop(){}
+}
---
+++ Team2993/src/main/java/org/firstinspires/ftc/team2993/robot_config.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.team2993;public class robot_config {
+}
